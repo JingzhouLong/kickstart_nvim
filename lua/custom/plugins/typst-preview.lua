052c5e806fdf1850f657b60d7212bd8fc81a4bf4
Jingzhou Long <ge49dev@mytum.de>
2025-10-26 15:19:12 +0100

    compat with other ditros with a symlink

--- lua/custom/plugins/typst-preview.lua
+++ lua/custom/plugins/typst-preview.lua
@@ -3,7 +3,7 @@ return {
   lazy = false, -- or ft = 'typst'
   version = '1.*',
   opts = {
-    open_cmd = 'firefox-bin %s -P typst-preview --class typst-preview',
+    open_cmd = 'firefox %s -P typst-preview --class typst-preview',
     dependencies_bin = { ['tinymist'] = 'tinymist' },
   }, -- lazy.nvim will implicitly calls `setup {}`
 }
